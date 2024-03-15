c73c9b6d1e12d6500b653d59dad2111d2771ebfd
Hein Htut Aung <heinhtutaung@Heins-iMac.local>
2024-03-15 16:56:41 +0630

    Finish

--- Forum/forum.js
+++ Forum/forum.js
@@ -6,28 +6,33 @@ let menu = document.getElementById("menu");
 let navigationBar2 = document.getElementById("nav-2");
 let contentContainer = document.getElementById("content-container");
 let currentDate = new Date();
+let nodeListUsers = document.querySelectorAll(".flex b");
+let elementArrays = [...nodeListUsers];
+let usernames = [];
+usernames = elementArrays.map(user => ({name: user.textContent}));
+let searchInput = document.querySelector(".search");
+let content = document.querySelectorAll(".content");
+let viewMore = document.querySelector(".view-more");
 
+// let monthNames = [
+//   "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+//   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+// ];
 
-let monthNames = [
-  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-];
+// let month = monthNames[currentDate.getMonth()];
+// let day = currentDate.getDate();
+// let year = currentDate.getFullYear();
+// let hours = currentDate.getHours();
+// let minutes = currentDate.getMinutes();
 
-let month = monthNames[currentDate.getMonth()];
-let day = currentDate.getDate();
-let year = currentDate.getFullYear();
-let hours = currentDate.getHours();
-let minutes = currentDate.getMinutes();
+// let amPm = hours >= 12 ? "PM" : "AM";
 
-let amPm = hours >= 12 ? "PM" : "AM";
+// hours = hours % 12;
+// hours = hours ? hours : 12;
 
-hours = hours % 12;
-hours = hours ? hours : 12;
+// let formattedTime = month + " " + day + ", " + year + " " + hours + ":" + (minutes < 10 ? "0" : "") + minutes + " " + amPm;
 
-let formattedTime = month + " " + day + ", " + year + " " + hours + ":" + (minutes < 10 ? "0" : "") + minutes + " " + amPm;
-
-// Output the current time
-console.log("Current Time: " + formattedTime);
+// console.log("Current Time: " + formattedTime);
 
 document.getElementById("wrap-events").addEventListener("click", () => {
     eventDropdown.classList.toggle("show");
@@ -78,39 +83,87 @@ document.getElementById("menu").addEventListener("click", () => {
 
 
 
-document.getElementById("post").addEventListener("click", (event) => {
-    event.preventDefault();
-    let division = document.createElement("div");
-    let flex1 = document.createElement("div");
-    let defaultPhoto = document.createElement("img");
-    let div1 = document.createElement("div");
-    let p1 = document.createElement("p");
-    let p2 = document.createElement("p");
-    let flex2 = document.createElement("div");
-    let bookImage = document.createElement("img");
-    let div2 = document.createElement("div");
-    let h4 = document.createElement("h4");
-    let paragraph = document.createElement("p");
-    let noReplies = document.createElement("p");
-    let topic = document.getElementById("topic").value;
-    let clubMessage = document.getElementById("club-message").value;
-    division.classList.add("content");
-    flex1.classList.add("flex");
-    defaultPhoto.setAttribute("src", "../Img/Default_pfp.svg.png");
-    defaultPhoto.classList.add("default-user")
-    p1.textContent = "User";
-    p2.textContent = formattedTime;
-    div1.append(p1, p2);
-    flex1.append(defaultPhoto, div1);
-    h4.textContent = topic;
-    paragraph.textContent = clubMessage;
-    div2.append(h4, paragraph);
-    flex2.classList.add("flex-2");
-    bookImage.setAttribute("src", "../Book Collection/Adventure10.jpeg");
-    flex2.append(bookImage, div2);
-    noReplies.textContent = "No replies yet";
-    noReplies.classList.add("no-replies");
-    division.append(flex1, flex2, noReplies);
-    document.getElementById("content-container").append(division);
+// document.getElementById("post").addEventListener("click", (event) => {
+//     event.preventDefault();
+//     let division = document.createElement("div");
+//     let flex1 = document.createElement("div");
+//     let defaultPhoto = document.createElement("img");
+//     let div1 = document.createElement("div");
+//     let p1 = document.createElement("p");
+//     let p2 = document.createElement("p");
+//     let flex2 = document.createElement("div");
+//     let bookImage = document.createElement("img");
+//     let div2 = document.createElement("div");
+//     let h4 = document.createElement("h4");
+//     let paragraph = document.createElement("p");
+//     let noReplies = document.createElement("p");
+//     let topic = document.getElementById("topic").value;
+//     let clubMessage = document.getElementById("club-message").value;
+//     division.classList.add("content");
+//     flex1.classList.add("flex");
+//     defaultPhoto.setAttribute("src", "../Img/Default_pfp.svg.png");
+//     defaultPhoto.classList.add("default-user")
+//     p1.textContent = "User";
+//     p2.textContent = formattedTime;
+//     div1.append(p1, p2);
+//     flex1.append(defaultPhoto, div1);
+//     h4.textContent = topic;
+//     paragraph.textContent = clubMessage;
+//     div2.append(h4, paragraph);
+//     flex2.classList.add("flex-2");
+//     bookImage.setAttribute("src", "../Book Collection/Adventure10.jpeg");
+//     flex2.append(bookImage, div2);
+//     noReplies.textContent = "No replies yet";
+//     noReplies.classList.add("no-replies");
+//     division.append(flex1, flex2, noReplies);
+//     document.getElementById("content-container").append(division);
+// });
+
+
+document.querySelector("#search").onclick = () => {
+    
+    let inputValue = searchInput.value.toLowerCase();
+    console.log(inputValue !== "")
+    if(inputValue !== "")
+    {
+        usernames.forEach((user, index) => {
+            console.log(user.name)
+            let isVisible = user.name.toLowerCase().includes(inputValue);
+            console.log(user.name.toLowerCase().includes(inputValue))
+            content[index].classList.toggle("hide", !isVisible);
+        });
+        viewMore.classList.add("hide");
+    }
+}
+
+document.querySelector(".search").onkeydown = (e) => {
+    if(e.key === "Enter")
+    {
+        document.querySelector("#search").onclick();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    searchInput.focus();
 });
 
+for(let i = 4; i < content.length; i++) 
+{
+    content[i].classList.add("hide");
+}
+let currentIndex = 4;
+
+document.querySelector(".view-more").addEventListener("click", () => {
+    let showMessageCount = currentIndex + 4;
+    for(let i = currentIndex; i < showMessageCount; i++)
+    {
+        console.log("click")
+        content[i].classList.remove("hide");
+        currentIndex++;
+        if(content[i+1] === undefined)
+        {
+            viewMore.classList.add("hide");
+        }
+    }
+  
+});
